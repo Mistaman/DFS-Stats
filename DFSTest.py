d074b9af81d14002fa4d701d11c7dc12689824ed
Mistaman <Mistaman666@gmail.com>
2015-10-30 12:08:06 -0400

    Added folder structure.

--- DFSTest.py
+++ DFSTest.py
@@ -6,11 +6,14 @@ import xlsxwriter   #https://pypi.python.org/pypi/XlsxWriter / http://xlsxwriter
 # http://www.footballoutsiders.com/stats/teamdef  Defensive Ranking website
 
 
-colNum = 3   #column a ltter of DFS spreadsheet
+defRowNum = 2   #column  of DFS spreadsheet
 rowNum = 2   #row number of DFS spreadsheet
 
-fdWB = openpyxl.load_workbook('FanDuelPlayerList.xlsx')   #read FD player stat spreadsheet
+fdWB = openpyxl.load_workbook('FanDuelPlayerList.xlsx')   #read FD player stat xlsx file
 fdSheet = fdWB.get_active_sheet()
+
+defenseWB = openpyxl.load_workbook('DefensiveRanking.xlsx')    #read defesive ranking xlsx file
+defenseSheet = defenseWB.get_active_sheet()
 
 dfsWB = xlsxwriter.Workbook('DFStats.xlsx')   #create xlsx file
 dfsSheet = dfsWB.add_worksheet('DFSTest')
@@ -21,19 +24,25 @@ greenCell = dfsWB.add_format({'bg_color': '#00FF7F'})   #springgreen #00FF7F hex
 blueCell = dfsWB.add_format({'bg_color': '#00BFFF'})   #deepskyblue #00BFFF hex code
 
 dfsSheet.write(0, 0, 'Player Team', bold)
-dfsSheet.write(0, 1, 'Opponent', bold)
-dfsSheet.write(0, 2, 'Position', bold)
-dfsSheet.write(0, 3, 'First Name', bold)
-dfsSheet.write(0, 4, 'Last Name', bold)
-dfsSheet.write(0, 5, 'FPPG', bold)
-dfsSheet.write(0, 6, 'FD Salary', bold)
-dfsSheet.write(0, 7, 'Projected Value', bold)
-dfsSheet.write(0, 8, 'Injury Status', bold)
-dfsSheet.write(0, 9, 'Injury Details', bold)
+dfsSheet.write(0, 1, 'Position', bold)
+dfsSheet.write(0, 2, 'First Name', bold)
+dfsSheet.write(0, 3, 'Last Name', bold)
+dfsSheet.write(0, 4, 'Opponent', bold)
+dfsSheet.write(0, 5, 'Opp. D. Pass Rank', bold)
+dfsSheet.write(0, 6, 'Opp. D. Rush Rank', bold)
+dfsSheet.write(0, 7, 'FPPG', bold)
+dfsSheet.write(0, 8, 'FD Salary', bold)
+dfsSheet.write(0, 9, 'Projected Value', bold)
+dfsSheet.write(0, 10, 'Injury Status', bold)
+dfsSheet.write(0, 11, 'Injury Details', bold)
 
 while rowNum <= fdSheet.get_highest_row():
+    defRowNum = 2
     playerTeam = fdSheet['I' + str(rowNum)].value
     playerOpp = fdSheet['J' + str(rowNum)].value
+    oppTeamName = defenseSheet['A' + str(defRowNum)].value
+    oppPassRank = defenseSheet['G'+ str(defRowNum)].value
+    oppRushRank = defenseSheet['I'+ str(defRowNum)].value
     playerPosition = fdSheet['B' + str(rowNum)].value
     playerFirstName = fdSheet['C' + str(rowNum)].value
     playerLastName = fdSheet['D' + str(rowNum)].value
@@ -44,24 +53,32 @@ while rowNum <= fdSheet.get_highest_row():
     projectedValue = ((fdFPPG.value / fdSalary.value) * 1000)   #FD projected value based on FPPG / Salary
 
     dfsSheet.write(rowNum-1, 0, playerTeam)   #write all relevant data to xlsx sheet
-    dfsSheet.write(rowNum-1, 1, playerOpp)
-    dfsSheet.write(rowNum-1, 2, str(playerPosition))
-    dfsSheet.write(rowNum-1, 3, str(playerFirstName))
-    dfsSheet.write(rowNum-1, 4, str(playerLastName))
-    dfsSheet.write(rowNum-1, 5, str(fdFPPG.value))
-    dfsSheet.write(rowNum-1, 6, str(fdSalary.value))
+    dfsSheet.write(rowNum-1, 1, str(playerPosition))
+    dfsSheet.write(rowNum-1, 2, str(playerFirstName))
+    dfsSheet.write(rowNum-1, 3, str(playerLastName))
+    dfsSheet.write(rowNum-1, 4, playerOpp)
+    
+    while defRowNum <= defenseSheet.get_highest_row():
+        if playerOpp != oppTeamName:
+            defRowNum += 1
+        else:
+            dfsSheet.write(rowNum-1, 5, oppPassRank)
+            dfsSheet.write(rowNum-1, 6, oppRushRank)
+        
+    dfsSheet.write(rowNum-1, 7, str(fdFPPG.value))
+    dfsSheet.write(rowNum-1, 8, str(fdSalary.value))
 
     if projectedValue > 2.25:
-        dfsSheet.write(rowNum-1, 7, projectedValue, greenCell)
+        dfsSheet.write(rowNum-1, 9, projectedValue, greenCell)
     elif projectedValue > 1.75:
-        dfsSheet.write(rowNum-1, 7, projectedValue, blueCell)
+        dfsSheet.write(rowNum-1, 9, projectedValue, blueCell)
     elif projectedValue < 1.25:
-        dfsSheet.write(rowNum-1, 7, projectedValue, redCell)
+        dfsSheet.write(rowNum-1, 9, projectedValue, redCell)
     else:
-        dfsSheet.write(rowNum-1, 7, projectedValue)
+        dfsSheet.write(rowNum-1, 9, projectedValue)
 
-    dfsSheet.write(rowNum-1, 8, injuryStatus)
-    dfsSheet.write(rowNum-1, 9, injuryDetails)
+    dfsSheet.write(rowNum-1, 10, injuryStatus)
+    dfsSheet.write(rowNum-1, 11, injuryDetails)
     rowNum += 1
 
 dfsWB.close()
